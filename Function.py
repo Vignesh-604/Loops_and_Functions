992a25acf0a25e852f5a4eddf74991ad7802dd5d
Vignesh H <110926281+Vignesh-604@users.noreply.github.com>
2022-08-29 00:21:48 +0530

    Updated 29/08/22

---
+++ Function.py
@@ -0,0 +1,12 @@
+def Func1():
+    print("Excecutes certain set of codes with reults when needed.")
+
+def Func2(animal, age, pet):
+    print("I know a " + animal + " who is " + str(age) + " and has a " + pet + " as a pet.")
+
+def Func3(animal, age, pet):
+    print(f"I know a {pet} who is {age} and has a {animal} as a pet.")
+
+Func2("Bear", 18, "cat")
+Func3("Monkey", "17", "Idiot")
+Func1()
